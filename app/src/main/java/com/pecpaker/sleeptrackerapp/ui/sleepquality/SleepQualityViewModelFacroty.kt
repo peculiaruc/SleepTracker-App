401fa5308cb47b22c2382b7722c859e97fa2dc9b
peculiaruc <peculiarumeh02@gmail.com>
2020-09-22 23:05:59 +0100

    Implemented Encapsulation

--- app/src/main/java/com/pecpaker/sleeptrackerapp/ui/sleepquality/SleepQualityViewModelFacroty.kt
+++ app/src/main/java/com/pecpaker/sleeptrackerapp/ui/sleepquality/SleepQualityViewModelFacroty.kt
@@ -1,4 +1,20 @@
 package com.pecpaker.sleeptrackerapp.ui.sleepquality
 
-class SleepQualityViewModelFacroty {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.pecpaker.sleeptrackerapp.dataSource.local.SleepDatabase
+import com.pecpaker.sleeptrackerapp.dataSource.local.SleepNightDao
+import java.lang.IllegalArgumentException
+
+class SleepQualityViewModelFacroty(
+    private val sleepNight: Long,
+    private val dataSource: SleepNightDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepQualityViewModel::class.java)) {
+            return SleepQualityViewModel(sleepNight, dataSource) as T
+        }
+        throw IllegalArgumentException("Unknown Viewmodel class")
+    }
+
 }
