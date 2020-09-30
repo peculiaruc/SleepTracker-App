ad0384b3ebd8abfeb5acd626b8312ca04e13df79
peculiaruc <peculiarumeh02@gmail.com>
2020-09-30 09:24:40 +0100

    Implemented SleepTrackerAdapter

--- app/src/main/java/com/pecpaker/sleeptrackerapp/Utli/Util.kt
+++ app/src/main/java/com/pecpaker/sleeptrackerapp/Utli/Util.kt
@@ -9,6 +9,8 @@ import androidx.core.text.HtmlCompat
 import com.pecpaker.sleeptrackerapp.R
 import com.pecpaker.sleeptrackerapp.dataSource.local.SleepNight
 import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -45,6 +47,29 @@ fun convertNumericQualityToString(quality: Int, resources: Resources): String {
 fun convertLongToDateString(systemTime: Long): String {
     return SimpleDateFormat("EEEE MMM-dd-yyyy' Time: 'HH:mm")
         .format(systemTime).toString()
+}
+
+private val ONE_MINUTE_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES)
+private val ONE_HOUR_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
+
+
+fun convertDurationToFormatted(startTimeMilli: Long, endTimeMilli: Long, res: Resources): String {
+    val durationMilli = endTimeMilli - startTimeMilli
+    val weekdayString = SimpleDateFormat("EEEE", Locale.getDefault()).format(startTimeMilli)
+    return when {
+        durationMilli < ONE_MINUTE_MILLIS -> {
+            val seconds = TimeUnit.SECONDS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.seconds_length, seconds, weekdayString)
+        }
+        durationMilli < ONE_HOUR_MILLIS -> {
+            val minutes = TimeUnit.MINUTES.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.minutes_length, minutes, weekdayString)
+        }
+        else -> {
+            val hours = TimeUnit.HOURS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.hours_length, hours, weekdayString)
+        }
+    }
 }
 
 /**
--- app/src/main/java/com/pecpaker/sleeptrackerapp/ui/sleepTraker/SleepTrackerAdapter.kt
+++ app/src/main/java/com/pecpaker/sleeptrackerapp/ui/sleepTraker/SleepTrackerAdapter.kt
@@ -4,9 +4,12 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.pecpaker.sleeptrackerapp.R
+import com.pecpaker.sleeptrackerapp.Utli.convertDurationToFormatted
+import com.pecpaker.sleeptrackerapp.Utli.convertNumericQualityToString
 import com.pecpaker.sleeptrackerapp.dataSource.local.SleepNight
 import kotlinx.android.synthetic.main.fragment_sleep_quality.view.*
 
@@ -21,27 +24,48 @@ class SleepTrackerAdapter : RecyclerView.Adapter<SleepTrackerAdapter.ViewHolder>
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        val textView = itemView.findViewById<TextView>(R.id.text_View)
+        // val textView = itemView.findViewById<TextView>(R.id.text_View)
+        val sleepLength = itemView.findViewById<TextView>(R.id.sleep_length)
+        val quality = itemView.findViewById<TextView>(R.id.quality_string)
+        val qualityImage = itemView.findViewById<ImageView>(R.id.quality_image)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val itemView = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        val itemView = layoutInflater.inflate(R.layout.list_sleep_night, parent, false)
         return ViewHolder(itemView)
     }
 
     override fun getItemCount() = data.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val itemView = data[position]
-        if (itemView.sleepQuality <= 1) {
-            holder.textView.setTextColor(Color.RED)
-        } else {
-            // reset
-            holder.textView.setTextColor(Color.BLACK)
-        }
-        holder.textView.text = itemView.sleepQuality.toString()
+        val item = data[position]
+        val res = holder.itemView.context.resources
+        holder.sleepLength.text =
+            convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+        holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
+        holder.qualityImage.setImageResource(
+            when (item.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+
+            }
+        )
+
+
+//        if (itemView.sleepQuality <= 1) {
+//            holder.textView.setTextColor(Color.RED)
+//        } else {
+//            // reset
+//            holder.textView.setTextColor(Color.BLACK)
+//        }
+//        holder.textView.text = itemView.sleepQuality.toString()
 
     }
 
